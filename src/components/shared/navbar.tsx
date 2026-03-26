aca51abf914352d415b78dad1ef2baf349748ae8
MVNSouza <marcos.nasouza2@gmail.com>
2026-03-26 02:46:29 -0300

    :sparkles: feat: refactor Navbar component layout and improve styling

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -3,47 +3,49 @@ import { Link } from "react-router"
 
 function Navbar() {
   return (
-    <header className="flex justify-between items-center min-w-svw p-6 bg-card/80 sticky top-0">
-      <div>
-        <Link to={"/"}>
-          <img
-            src="src\assets\img\logo.png"
-            alt="Logo Skyfall RPG"
-            className="h-14"
-          />
-        </Link>
-      </div>
-      <ul className="flex gap-8">
-        <li>
-          <Link to={"/"} className="hover:text-primary font-semibold">
-            Início
+    <header className="sticky top-0 border-b border-border z-50 bg-card/80 backdrop-blur-md">
+      <nav className="container-main flex items-center justify-between py-4 px-4">
+        <div>
+          <Link to={"/"}>
+            <img
+              src="src\assets\img\logo.png"
+              alt="Logo Skyfall RPG"
+              className="h-14"
+            />
           </Link>
-        </li>
-        <li>
-          <Link
-            to={"/#feature-section"}
-            className="hover:text-primary font-semibold"
-          >
-            Recursos
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={"/#skyfall-section"}
-            className="hover:text-primary font-semibold"
-          >
-            Skyfall RPG
-          </Link>
-        </li>
-      </ul>
-      <div>
-        <Button variant={"ghost"} size="lg" asChild>
-          <Link to="/login">Entrar</Link>
-        </Button>
-        <Button size={"lg"} asChild>
-          <Link to={"/register"}>Criar conta</Link>
-        </Button>
-      </div>
+        </div>
+        <ul className="flex gap-4 lg:gap-8 text-lg">
+          <li>
+            <Link to={"/"} className="hover:text-primary font-semibold">
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"/#feature-section"}
+              className="hover:text-primary font-semibold"
+            >
+              Recursos
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"/#skyfall-section"}
+              className="hover:text-primary font-semibold"
+            >
+              Skyfall RPG
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <Button variant={"ghost"} size="lg" asChild>
+            <Link to="/login">Entrar</Link>
+          </Button>
+          <Button size={"lg"} asChild>
+            <Link to={"/register"}>Criar conta</Link>
+          </Button>
+        </div>
+      </nav>
     </header>
   )
 }
